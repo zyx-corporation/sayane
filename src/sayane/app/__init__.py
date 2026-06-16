67c0665f8201cc7ccf384521cd93d3a2580197d4
Tomoyuki Kano <tomyuk@zyxcorp.jp>
2026-06-16 09:19:20 +0900

    feat(app): export resident UI skeleton helpers

--- src/sayane/app/__init__.py
+++ src/sayane/app/__init__.py
@@ -3,11 +3,14 @@
 from sayane.app.capabilities import CapabilityToken, create_local_capability_token
 from sayane.app.runtime import ResidentRuntime, build_resident_runtime
 from sayane.app.service import ResidentAppService
+from sayane.app.ui import build_mcp_preview, build_review_queue
 
 __all__ = [
     "CapabilityToken",
     "ResidentAppService",
     "ResidentRuntime",
+    "build_mcp_preview",
     "build_resident_runtime",
+    "build_review_queue",
     "create_local_capability_token",
 ]
